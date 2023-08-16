2fb42a18cb9c1e71cd8c3dec6cea97064b7b9ad2
Roger-Indrawan <142372125+Roger-Indrawan@users.noreply.github.com>
2023-08-16 11:10:25 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,1 +1,34 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 
+# Web App Title
+st.markdown('''
+# **The EDA App**
+
+This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
+
+---
+''')
+
+# Upload CSV data
+with st.sidebar.header('Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+[Example CSV input file](https://www.kaggle.com/datasets/uciml/pima-indians-diabetes-database)
+""")
+
+# Pandas Profiling Report
+if uploaded_file is not None:
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
